cd2052c340d1ee98b1f44f499b8b5191db665d59
杨超越 <62123934+MTpupil@users.noreply.github.com>
2022-03-16 15:28:42 +0800

    Update GitMind.js

--- GitMind.js
+++ GitMind.js
@@ -1,5 +1,5 @@
 /*
 
 */
-body = $response.body.replace(/is_vip\":\\d"/g, 'is_vip\":1').replace(/vip_expired_at\":\".*?\"/g, 'vip_expired_at\":\"2099-12-31 00:00\"');
+body = $response.body.replace(/is_vip\":\d"/g, 'is_vip\":1').replace(/vip_expired_at\":\".*?\"/g, 'vip_expired_at\":\"2099-12-31 00:00\"');
 $done({body});
