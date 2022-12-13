aef8ce971ece892c1a0349d0dfd2345a05fdfb8f
杨超越 <62123934+MTpupil@users.noreply.github.com>
2022-12-13 14:46:05 +0800

    Create yunshangxiezuo.js

---
+++ yunshangxiezuo.js
@@ -0,0 +1,21 @@
+/**
+ * 云上写作解锁会员功能
+ * 公众号：木瞳科技Pro
+ * [MITM]
+ * hostname = yunshangxiezuo.com
+ * 
+ * Quantumult X
+ * [rewrite_local]
+ * ^https?:\/\/paint\.api\.wombo\.ai:443\/api\/(users|styles|premium\/sync) url script-response-body https://raw.githubusercontent.com/MTpupil/quantumultx/main/Dream.js
+ * 
+ */
+
+const SCRIPT_NAME='云上写作';
+const user=/http:\/\/www\.yunshangxiezuo\.com\/api\/getAuthenticate_2/;
+if(user.test($request.url)){
+let obj=JSON.parse($response.body);
+obj.data.user["name"]="木瞳科技Pro";
+obj.data.user["vip"]=1;
+obj.data.user["vip_forever"]=1;
+let body=JSON.stringify(obj);
+$done({body})}
