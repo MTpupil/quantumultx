5a61429c0b9e77fcc337f9251b40146e300f073e
杨超越 <62123934+MTpupil@users.noreply.github.com>
2022-12-14 08:18:02 +0800

    Update yunshangxiezuo2.js

--- yunshangxiezuo2.js
+++ yunshangxiezuo2.js
@@ -1,2 +1,3 @@
-body = $response.body.replace(/vip\":\d"/g, 'vip\":1').replace(/vip_forever\":\d/g, "vip_forever':1")
+let a = $response.body;
+let body = a.replace(/vip\":\d"/g, 'vip\":1').replace(/vip_forever\":\d/g, "vip_forever':1")
 $done({body});
