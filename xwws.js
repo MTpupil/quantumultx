a10cab2cda08a85adb7444681061d95938f4e019
杨超越 <62123934+MTpupil@users.noreply.github.com>
2022-12-18 13:10:57 +0800

    Update xwws.js

--- xwws.js
+++ xwws.js
@@ -11,14 +11,11 @@
  */
 
 const SCRIPT_NAME='小歪微商';
-const user=/https?:\/\/xy\.jietuguanjia\.com\/api\/app\/userInfo/;
+const user=/http:\/\/xy\.jietuguanjia\.com\/api\/app\/userInfo/;
 if(user.test($request.url)){
 let obj=JSON.parse($response.body);
 obj.data["username"]="木瞳科技Pro";
 obj.data["head"]="https://s1.ax1x.com/2022/11/23/z8LIPO.jpg";
 obj.data["isVip"]=true;
-obj.data["isInAppBuy"]=1;
-obj.data["status"]=1;
-obj.data["vipExpiredTime"]=4999999999;
 let body=JSON.stringify(obj);
 $done({body})}
