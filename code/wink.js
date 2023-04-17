d5daf1cfef0ec0465cc34a89922f97e5d56ba453
杨超越 <62123934+MTpupil@users.noreply.github.com>
2023-04-17 14:42:21 +0800

    更新 wink.js

--- code/wink.js
+++ code/wink.js
@@ -1,3 +1,4 @@
+
 /**
  * wink开启会员功能
  * 公众号：木瞳科技Pro
@@ -12,29 +13,32 @@
  */
 
 const SCRIPT_NAME='Wink';
-const vip= /https:\/\/api-sub\.meitu\.com\/v2\/user\/vip_info_by_group\.json/;
-const user = /https:\/\/api(\.account|-winkcut)\.meitu\.com\/users?\/show(_current)?\.json/;
-
+const vip= /https:\/\/api-sub\.meitu\.com\/v2\/user\/vip_info_by_group\.json/
+const user = /https:\/\/api(\.account|-winkcut)\.meitu\.com\/users?\/show(_current)?\.json/
 
 
 if(user.test($request.url)){
   let obj=JSON.parse($response.body);
+if(obj.data!=null){
   obj.data["screen_name"]="木瞳科技Pro";
   obj.data["desc"]="公众号：木瞳科技Pro";
   obj.data["vip_type"]=1;
+}
+if(obj.response!=null){
   obj.response.user["screen_name"]="木瞳科技Pro";
   obj.response.user["blue_v_status"]=1;
   obj.response.user["red_v_status"]=1;
+}
   let body=JSON.stringify(obj);
   $done({body})
+
 }
 
 if(vip.test($request.url)){
   let obj=JSON.parse($response.body);
 
-  obj.data["is_vip"]=ture;
+  obj.data["is_vip"]=true;
   obj.data["use_vip"]=true;
- 
   
   let body=JSON.stringify(obj);
   $done({body})
