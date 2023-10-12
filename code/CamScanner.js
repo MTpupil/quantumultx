1cacca39a03980c6702940e1a104b69fd5acecf8
杨超越 <62123934+MTpupil@users.noreply.github.com>
2023-10-12 19:45:21 +0800

    Create CamScanner.js

---
+++ code/CamScanner.js
@@ -0,0 +1,17 @@
+/*
+CamScanner 解锁部分高级特权
+
+***************************
+Quantumult X:
+
+[rewrite_local]
+^https:\/\/(api|api-cs)\.intsig\.net\/purchase\/cs\/query_property\? url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/CamScanner.js
+
+[mitm]
+hostname = ap*.intsig.net
+
+***************************/
+
+let obj = JSON.parse($response.body);
+obj = {"data":{"psnl_vip_property":{"expiry":"32503564800"}}};
+$done({body: JSON.stringify(obj)});
